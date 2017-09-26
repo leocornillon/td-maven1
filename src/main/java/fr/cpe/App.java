31f600992005f0ffcd15da0784602e485bbcccca
leoco <corle.cugly@gmail.com>
2017-09-26 10:58:13 +0200

    Premier commit

---
+++ src/main/java/fr/cpe/App.java
@@ -0,0 +1,16 @@
+package fr.cpe;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        DateTime now = new DateTime();
+    }
+}
